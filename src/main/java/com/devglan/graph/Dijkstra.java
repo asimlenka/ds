f14ef6ff8eeb99960ecd918bd1dd531b14490da2
only2 <only2dhir@gmail.com>
2020-04-06 01:24:52 +0530

    dijkstra implementation

--- src/main/java/com/devglan/graph/Dijkstra.java
+++ src/main/java/com/devglan/graph/Dijkstra.java
@@ -2,91 +2,65 @@ package com.devglan.graph;
 
 public class Dijkstra {
 
-    // A utility function to find the vertex with minimum distance value,
-    // from the set of vertices not yet included in shortest path tree
-    static final int V = 9;
-    int minDistance(int dist[], Boolean sptSet[])
-    {
-        // Initialize min value
-        int min = Integer.MAX_VALUE, min_index = -1;
-
-        for (int v = 0; v < V; v++)
-            if (sptSet[v] == false && dist[v] <= min) {
-                min = dist[v];
-                min_index = v;
+    public static void dijkstra(int[][] graph, int sourceVertex){
+        int vertexCount = graph.length;
+        boolean[] visitedVertex = new boolean[vertexCount];
+        int[] distance = new int[vertexCount];
+        for (int i = 0; i < vertexCount; i++){
+            visitedVertex[i] = false;
+            distance[i] = Integer.MAX_VALUE;
+        }
+        distance[sourceVertex] = 0; // distance of source vertex to itself is zero
+        for (int i = 0; i < vertexCount; i++){
+            //find the neighbouring unvisited vertex having  minimum distance from source vertex
+            //for the first time the minDistanceVertexIndex will be the source vertex and the distance array will be updated with the neighbouring vertex distance of source vertex
+            int u = findMinDistance(distance, visitedVertex);
+            //u is the row and v is the column
+            visitedVertex[u] = true;
+            //as per the formula minDistanceVertexIndex is u
+            //now update all the neighbour vertex distances
+            //to update distance of neighbour vertex, traverse all the neighbours of this vertex and in matrix representation the neighbour of this min
+            //vertex are present in the same row. Hence, iterate each element of the row(meaning column wise for this row)
+            //and find the distance
+            for (int v =0 ; v < vertexCount; v++){
+            //graph[minDistanceVertexIndex][v] != 0 -> there should be a direct edge
+                if(!visitedVertex[v] && graph[u][v] != 0 && (distance[u] + graph[u][v] < distance[v])){
+                    distance[v] = distance[u] + graph[u][v];
+                }
             }
-
-        return min_index;
-    }
-
-    // A utility function to print the constructed distance array
-    void printSolution(int dist[])
-    {
-        System.out.println("Vertex \t\t Distance from Source");
-        for (int i = 0; i < V; i++)
-            System.out.println(i + " \t\t " + dist[i]);
-    }
-
-    // Function that implements Dijkstra's single source shortest path
-    // algorithm for a graph represented using adjacency matrix
-    // representation
-    void dijkstra(int graph[][], int src)
-    {
-        int dist[] = new int[V]; // The output array. dist[i] will hold
-        // the shortest distance from src to i
-
-        // sptSet[i] will true if vertex i is included in shortest
-        // path tree or shortest distance from src to i is finalized
-        Boolean sptSet[] = new Boolean[V];
-
-        // Initialize all distances as INFINITE and stpSet[] as false
-        for (int i = 0; i < V; i++) {
-            dist[i] = Integer.MAX_VALUE;
-            sptSet[i] = false;
+        }
+        for (int i = 0; i < distance.length; i++){
+            System.out.println(String.format("Distance from source vertex %s to vertex %s is %s", sourceVertex, i, distance[i]));
         }
 
-        // Distance of source vertex from itself is always 0
-        dist[src] = 0;
-
-        // Find shortest path for all vertices
-        for (int count = 0; count < V - 1; count++) {
-            // Pick the minimum distance vertex from the set of vertices
-            // not yet processed. u is always equal to src in first
-            // iteration.
-            int u = minDistance(dist, sptSet);
-
-            // Mark the picked vertex as processed
-            sptSet[u] = true;
-
-            // Update dist value of the adjacent vertices of the
-            // picked vertex.
-            for (int v = 0; v < V; v++)
-
-                // Update dist[v] only if is not in sptSet, there is an
-                // edge from u to v, and total weight of path from src to
-                // v through u is smaller than current value of dist[v]
-                if (!sptSet[v] && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
-                    dist[v] = dist[u] + graph[u][v];
-        }
-
-        // print the constructed distance array
-        printSolution(dist);
     }
 
-    // Driver method
+    private static int findMinDistance(int[] distance, boolean[] visitedVertex) {
+        int minDistance = Integer.MAX_VALUE;
+        int minDistanceVertex = -1;
+        for (int i =0; i < distance.length; i++){
+            //the vertex should not be visited and the distance should be the minimum.
+            //for the first time, only the source vertex will match this condition and it will be returned as all other vertex has a distance of infinity
+            // from next iteration the minimum distance vertex will be returned. Even if a source vertex has multiple edges,
+            // this will return the minimum distance vertex
+            //this is similar to finding the min element of an array
+            if(!visitedVertex[i] && distance[i] < minDistance){
+                minDistance = distance[i];
+                minDistanceVertex = i;
+            }
+        }
+        return minDistanceVertex;
+    }
+
     public static void main(String[] args)
     {
-        /* Let us create the example graph discussed above */
-        int graph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+        //https://i.imgur.com/6R8oPO1.png
+        int graph[][] = new int[][] { { 0, 4, 8, 0, 0 },
+                { 4, 0, 2, 5, 0 },
+                { 8, 2, 0, 5, 9},
+                { 0, 5, 5, 0, 4 },
+                { 0, 0, 9, 4, 0 } };
         Dijkstra t = new Dijkstra();
-        t.dijkstra(graph, 7);
+        t.dijkstra(graph, 0);
     }
 }
